260079a229870e58f070bb3285d6641264c5ef7f
Katarina Krstin <92788177+kkatarinna@users.noreply.github.com>
2023-11-19 20:08:31 +0100

    Starting project

---
+++ BookinAppTeam18/src/main/java/BookingAppTeam18.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.informatika.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppTeam18 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingAppTeam18.class, args);
+	}
+
+}
