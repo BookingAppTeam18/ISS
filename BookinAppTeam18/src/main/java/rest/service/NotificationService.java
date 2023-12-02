0034bc7fc8ca8d0fe20ed2dc8e67e5618219f8cf
JovanSV64 <vuckovic.sv64.2021@uns.ac.rs>
2023-12-02 13:08:02 +0100

    base Notification service, report service

--- BookinAppTeam18/src/main/java/rest/service/NotificationService.java
+++ BookinAppTeam18/src/main/java/rest/service/NotificationService.java
@@ -1,59 +1,100 @@
 package rest.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import rest.domain.DTO.NotificationDTO;
 import rest.domain.Notification;
-import rest.repository.CommentRepository;
 import rest.repository.NotificationRepository;
 
-import java.util.Collection;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.*;
 
 @Service
 public class NotificationService implements IService<NotificationDTO> {
 
     @Autowired
     private NotificationRepository notificationRepository;
+    ResourceBundle bundle = ResourceBundle.getBundle("ValidationMessages", LocaleContextHolder.getLocale());
     @Override
     public Collection<NotificationDTO> findAll() {
-        return null;
+
+        ArrayList<NotificationDTO> notificationsDTO = new ArrayList<>();
+        for(Notification notification:notificationRepository.findAll()){
+            notificationsDTO.add(new NotificationDTO(notification));
+        }
+        return notificationsDTO;
     }
 
     @Override
-    public NotificationDTO findOne(Long id) {
-        return null;
+    public NotificationDTO findOne(Long id)
+    {
+        Optional<Notification> found = notificationRepository.findById(id);
+        if (found.isEmpty()) {
+            String value = bundle.getString("notFound");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, value);
+        }
+        return new NotificationDTO(found.get());
     }
 
     @Override
-    public NotificationDTO insert(NotificationDTO object) throws Exception {
-        return null;
+    public NotificationDTO insert(NotificationDTO NotificationDTO){
+        Notification notification = new Notification(NotificationDTO);
+        try {
+            notificationRepository.save(notification);
+            notificationRepository.flush();
+            return NotificationDTO;
+        } catch (ConstraintViolationException ex) {
+            Set<ConstraintViolation<?>> errors = ex.getConstraintViolations();
+            StringBuilder sb = new StringBuilder(1000);
+            for (ConstraintViolation<?> error : errors) {
+                sb.append(error.getMessage() + "\n");
+            }
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, sb.toString());
+        }
     }
 
     @Override
-    public NotificationDTO update(NotificationDTO object) throws Exception {
-        return null;
+    public NotificationDTO update(NotificationDTO NotificationDTO) throws Exception {
+        Notification notificationToUpdate = new Notification(NotificationDTO);
+        try {
+            findOne(NotificationDTO.getId()); // this will throw ResponseStatusException if student is not found
+            notificationRepository.save(notificationToUpdate);
+            notificationRepository.flush();
+            return NotificationDTO;
+        } catch (RuntimeException ex) {
+            Throwable e = ex;
+            Throwable c = null;
+            while ((e != null) && !((c = e.getCause()) instanceof ConstraintViolationException) ) {
+                e = (RuntimeException) c;
+            }
+            if ((c != null) && (c instanceof ConstraintViolationException)) {
+                ConstraintViolationException c2 = (ConstraintViolationException) c;
+                Set<ConstraintViolation<?>> errors = c2.getConstraintViolations();
+                StringBuilder sb = new StringBuilder(1000);
+                for (ConstraintViolation<?> error : errors) {
+                    sb.append(error.getMessage() + "\n");
+                }
+                throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, sb.toString());
+            }
+            throw ex;
+        }
     }
 
     @Override
     public NotificationDTO delete(Long id) {
-        return null;
+        Notification found = new Notification(findOne(id)); // this will throw StudentNotFoundException if student is not found
+        notificationRepository.delete(found);
+        notificationRepository.flush();
+        return new NotificationDTO(found);
     }
-
 
     @Override
     public void deleteAll() {
-
-    }
-
-    public Collection<NotificationDTO> findAccountUnseenNotifications(Long accountId) {
-        return null;
-    }
-
-    public NotificationDTO setSeen(Long notificationId) {
-        return null;
-    }
-
-    public Collection<NotificationDTO> findAccountNotifications(Long accountId) {
-        return null;
+        notificationRepository.deleteAll();
+        notificationRepository.flush();
     }
 }
--- BookinAppTeam18/src/main/java/rest/service/ReportService.java
+++ BookinAppTeam18/src/main/java/rest/service/ReportService.java
@@ -1,56 +1,105 @@
 package rest.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import rest.domain.Report;
+import rest.domain.DTO.ReportDTO;
 import rest.domain.DTO.ReportDTO;
 import rest.domain.Report;
+import rest.domain.enumerations.Page;
 import rest.repository.CommentRepository;
+import rest.repository.ReportRepository;
 
-import java.util.Collection;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.*;
 
 @Service
 public class ReportService implements IService<ReportDTO>{
 
     @Autowired
-    private CommentRepository commentRepository;
-
-
+    private ReportRepository reportRepository;
+    ResourceBundle bundle = ResourceBundle.getBundle("ValidationMessages", LocaleContextHolder.getLocale());
     @Override
     public Collection<ReportDTO> findAll() {
-        return null;
+
+        ArrayList<ReportDTO> reportsDTO = new ArrayList<>();
+        for(Report report:reportRepository.findAll()){
+            reportsDTO.add(new ReportDTO(report));
+        }
+        return reportsDTO;
     }
 
     @Override
-    public ReportDTO findOne(Long id) {
-        return null;
+    public ReportDTO findOne(Long id)
+    {
+        Optional<Report> found = reportRepository.findById(id);
+        if (found.isEmpty()) {
+            String value = bundle.getString("notFound");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, value);
+        }
+        return new ReportDTO(found.get());
     }
 
     @Override
-    public ReportDTO insert(ReportDTO object) throws Exception {
-        return null;
+    public ReportDTO insert(ReportDTO reportDTO){
+        Report report = new Report(reportDTO);
+        try {
+            reportRepository.save(report);
+            reportRepository.flush();
+            return reportDTO;
+        } catch (ConstraintViolationException ex) {
+            Set<ConstraintViolation<?>> errors = ex.getConstraintViolations();
+            StringBuilder sb = new StringBuilder(1000);
+            for (ConstraintViolation<?> error : errors) {
+                sb.append(error.getMessage() + "\n");
+            }
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, sb.toString());
+        }
     }
 
     @Override
-    public ReportDTO update(ReportDTO object) throws Exception {
-        return null;
+    public ReportDTO update(ReportDTO reportDTO) throws Exception {
+        Report reportToUpdate = new Report(reportDTO);
+        try {
+            findOne(reportDTO.getId()); // this will throw ResponseStatusException if student is not found
+            reportRepository.save(reportToUpdate);
+            reportRepository.flush();
+            return reportDTO;
+        } catch (RuntimeException ex) {
+            Throwable e = ex;
+            Throwable c = null;
+            while ((e != null) && !((c = e.getCause()) instanceof ConstraintViolationException) ) {
+                e = (RuntimeException) c;
+            }
+            if ((c != null) && (c instanceof ConstraintViolationException)) {
+                ConstraintViolationException c2 = (ConstraintViolationException) c;
+                Set<ConstraintViolation<?>> errors = c2.getConstraintViolations();
+                StringBuilder sb = new StringBuilder(1000);
+                for (ConstraintViolation<?> error : errors) {
+                    sb.append(error.getMessage() + "\n");
+                }
+                throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, sb.toString());
+            }
+            throw ex;
+        }
     }
 
     @Override
     public ReportDTO delete(Long id) {
-        return null;
+        Report found = new Report(findOne(id)); // this will throw StudentNotFoundException if student is not found
+        reportRepository.delete(found);
+        reportRepository.flush();
+        return new ReportDTO(found);
     }
-
 
     @Override
     public void deleteAll() {
-        
+        reportRepository.deleteAll();
+        reportRepository.flush();
     }
 
-    public Collection<ReportDTO> findAccountReports(Long accountId) {
-        return null;
-    }
-
-    public Collection<ReportDTO> findCommentReports(Long accountId) {
-        return null;
-    }
 }
