1a6cb8161c30636ffa596a53932a2fa119e580d7
Katarina Krstin <92788177+kkatarinna@users.noreply.github.com>
2023-12-05 22:54:39 +0100

    Added query

--- BookinAppTeam18/src/main/java/rest/repository/PriceRepository.java
+++ BookinAppTeam18/src/main/java/rest/repository/PriceRepository.java
@@ -1,7 +1,16 @@
 package rest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import rest.domain.Comment;
 import rest.domain.Price;
+import rest.domain.enumerations.Page;
+
+import java.util.Collection;
 
 public interface PriceRepository extends JpaRepository<Price,Long> {
+
+    @Query("select p from Price p where p.accommodation.id= ?1")
+    public Collection<Comment> findPricesForAccommodation(Long accommodationId);
+
 }
