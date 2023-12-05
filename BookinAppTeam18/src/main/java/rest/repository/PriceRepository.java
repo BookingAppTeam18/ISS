31bee4640598c164b3db2128a07dfbfb2f2d3a92
Katarina Krstin <92788177+kkatarinna@users.noreply.github.com>
2023-12-05 23:35:25 +0100

    cleaned

--- BookinAppTeam18/src/main/java/rest/repository/PriceRepository.java
+++ BookinAppTeam18/src/main/java/rest/repository/PriceRepository.java
@@ -2,10 +2,7 @@ package rest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import rest.domain.Comment;
 import rest.domain.Price;
-import rest.domain.enumerations.Page;
-
 import java.util.Collection;
 
 public interface PriceRepository extends JpaRepository<Price,Long> {
