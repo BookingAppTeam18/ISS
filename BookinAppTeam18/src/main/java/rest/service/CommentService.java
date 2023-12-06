bb85420b2858e9d50645529da48771bf5eef9f9c
Katarina Krstin <92788177+kkatarinna@users.noreply.github.com>
2023-12-06 15:43:49 +0100

    unused import

--- BookinAppTeam18/src/main/java/rest/service/CommentService.java
+++ BookinAppTeam18/src/main/java/rest/service/CommentService.java
@@ -1,7 +1,6 @@
 package rest.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
