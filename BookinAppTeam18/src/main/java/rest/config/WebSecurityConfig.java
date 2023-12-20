561329bbf3a6a3155a97fe610139af4fd4c1490e
JovanSV64 <vuckovic.sv64.2021@uns.ac.rs>
2023-12-20 17:56:05 +0100

    commenting authorities for development

--- BookinAppTeam18/src/main/java/rest/config/WebSecurityConfig.java
+++ BookinAppTeam18/src/main/java/rest/config/WebSecurityConfig.java
@@ -88,7 +88,7 @@ public class WebSecurityConfig {
                 // ukoliko ne zelimo da koristimo @PreAuthorize anotacije nad metodama kontrolera, moze se iskoristiti hasRole() metoda da se ogranici
                 // koji tip korisnika moze da pristupi odgovarajucoj ruti. Npr. ukoliko zelimo da definisemo da ruti 'admin' moze da pristupi
                 // samo korisnik koji ima rolu 'ADMIN', navodimo na sledeci nacin:
-                .antMatchers("/api/accommodations").hasAuthority("OWNER") //ili .antMatchers("/admin").hasAuthority("ROLE_ADMIN")
+                //.antMatchers("/api/accommodations").hasAuthority("OWNER") //ili .antMatchers("/admin").hasAuthority("ROLE_ADMIN")
 
                 // za svaki drugi zahtev korisnik mora biti autentifikovan
                 .anyRequest().authenticated()
