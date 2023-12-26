875c425d14a3e137215e4403768b18f9f2282a94
Katarina Krstin <92788177+kkatarinna@users.noreply.github.com>
2023-12-26 23:55:30 +0100

    deleted accommodation request class

--- BookinAppTeam18/src/main/java/rest/repository/AccountCommentRepository.java
+++ BookinAppTeam18/src/main/java/rest/repository/AccountCommentRepository.java
@@ -2,7 +2,6 @@ package rest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import rest.domain.AccommodationRequest;
 import rest.domain.AccountComment;
 
 import java.util.Collection;
