467ad3548b8b32ae651db797785c1e9d65f2f3d0
JovanSV64 <vuckovic.sv64.2021@uns.ac.rs>
2024-01-13 23:15:13 +0100

    image resizing

--- BookinAppTeam18/src/main/java/rest/service/ImageService.java
+++ BookinAppTeam18/src/main/java/rest/service/ImageService.java
@@ -1,12 +1,15 @@
 package rest.service;
 
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import rest.domain.Accommodation;
 import rest.repository.AccommodationRepository;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -14,12 +17,12 @@ import java.util.List;
 
 @Service
 public class ImageService {
+
     @Autowired
     AccommodationRepository accommodationRepository;
+
     public void saveImages(List<MultipartFile> files, Long accommodationId) throws IOException {
-
         Accommodation accommodation = accommodationRepository.getOne(accommodationId);
-
         List<String> fileUrls = new ArrayList<>();
 
         String relativePath = "BookinAppTeam18/src/main/resources/static/";
@@ -29,24 +32,38 @@ public class ImageService {
 
         for (MultipartFile file : files) {
             String fileName = file.getOriginalFilename();
-            Path filePath = absolutePath.resolve(fileName);
-            accommodation.getGallery().add(filePath.toString());
-            System.out.println(filePath);
+            String baseFileName = fileName.substring(0, fileName.lastIndexOf('.'));
+            String fileExtension = fileName.substring(fileName.lastIndexOf('.'));
 
+            // Prilagodite rezoluciju slike za malu sliku
+            int smallWidth = 200;
+            int smallHeight = 200;
+            File smallFile = resizeImage(file, baseFileName + "_small" + fileExtension, absolutePath, smallWidth, smallHeight);
+            accommodation.getGallery().add(smallFile.getAbsolutePath());
 
-            // Sačuvajte fajl na željenu putanju
-            file.transferTo(filePath.toFile());
+            // Prilagodite rezoluciju slike za veliku sliku
+            int bigWidth = 400;
+            int bigHeight = 400;
+            File bigFile = resizeImage(file, baseFileName + "_big" + fileExtension, absolutePath, bigWidth, bigHeight);
+            accommodation.getGallery().add(bigFile.getAbsolutePath());
 
-            // Dodajte URL fajla u listu
-            fileUrls.add(filePath.toString());
-
-
-
+            // Sačuvajte informacije o smeštaju
             accommodationRepository.save(accommodation);
             accommodationRepository.flush();
         }
+    }
 
+    private File resizeImage(MultipartFile file, String fileName, Path absolutePath, int width, int height) throws IOException {
+        File tempFile = Files.createTempFile("temp-", null).toFile();
+        file.transferTo(tempFile);
 
+        Path filePath = absolutePath.resolve(fileName);
+
+        Thumbnails.of(tempFile)
+                .size(width, height)
+                .toFile(filePath.toFile());
+
+        return filePath.toFile();
     }
 
     public ArrayList<String> getAccommodationImages(Long accommodationId) {
