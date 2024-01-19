d6f29c9b7364039888bcb370695bf56875c9d4aa
ZivanicBojan <zivanic.sv61.2021@uns.ac.rs>
2024-01-19 23:43:45 +0100

    Added ReservationRepositoryTest

---
+++ BookinAppTeam18/src/test/java/rest/student2/repository/ReservationRepositoryTest.java
@@ -0,0 +1,38 @@
+package rest.student2.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import rest.domain.Price;
+import rest.domain.Reservation;
+import rest.domain.enumerations.ReservationStatus;
+import rest.repository.ReservationRepository;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class ReservationRepositoryTest {
+    @Autowired
+    private ReservationRepository reservationRepository;
+    @Test
+    public void shouldSavePrice(){
+        Date mockStartDate = new Date();
+        Date mockEndDate = new Date(mockStartDate.getTime());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mockEndDate);
+        calendar.add(Calendar.DAY_OF_MONTH, 7);
+        mockEndDate = calendar.getTime();
+
+        Reservation reservation = new Reservation(1L, mockStartDate, mockEndDate, 400.0, 3L, 3L, 2, ReservationStatus.APPROVED);
+
+        Reservation savedReservation = reservationRepository.save(reservation);
+        
+        assertThat(savedReservation).usingRecursiveComparison().isEqualTo(reservation);
+
+    }
+}
