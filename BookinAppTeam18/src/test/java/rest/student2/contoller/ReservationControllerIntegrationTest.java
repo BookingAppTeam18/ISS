8d7b0efa37724967b6aa4e29c3846ee1bf823a91
ZivanicBojan <zivanic.sv61.2021@uns.ac.rs>
2024-01-19 22:19:28 +0100

    Added jwt token

--- BookinAppTeam18/src/test/java/rest/student2/contoller/ReservationControllerIntegrationTest.java
+++ BookinAppTeam18/src/test/java/rest/student2/contoller/ReservationControllerIntegrationTest.java
@@ -2,68 +2,85 @@ package rest.student2.contoller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
+import rest.domain.DTO.PriceDTO;
 import rest.domain.DTO.ReservationDTO;
 import rest.domain.enumerations.ReservationStatus;
+import rest.utils.TokenUtils;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
     @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//    @ActiveProfiles("test")
     public class ReservationControllerIntegrationTest{
 
         @Autowired
         private TestRestTemplate restTemplate;
+        @Autowired
+        private TokenUtils tokenUtils;
 
-        @Test
+        @ParameterizedTest
+        @MethodSource("provideTestCases")
         @DisplayName("Should change reservation status")
-//        @WithUserDetails("owner")
-        public void shouldChangeReservationStatus() {
-            // Prvo, kreirajte rezervaciju koju želite ažurirati
-//            ReservationDTO initialReservation = restTemplate.postForObject("/api/reservations",
-//                    createSampleReservation(), ReservationDTO.class);
-            ReservationDTO reservationDTO = new ReservationDTO(1L, null, null, 100.0, 1L, 1L, 1, ReservationStatus.CREATED);
+        public void shouldChangeReservationStatus(Long reservationId, int expectedStatusCode) {
+            Date mockStartDate = new Date();
+            Date mockEndDate = new Date(mockStartDate.getTime());
 
-            HttpEntity<ReservationDTO> requestEntity = new HttpEntity<ReservationDTO>(reservationDTO);
+            // Dodajte nedelju dana na datum završetka
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(mockEndDate);
+            calendar.add(Calendar.DAY_OF_MONTH, 7);
+            mockEndDate = calendar.getTime();
 
 
-            ResponseEntity<ReservationDTO> responseEntity = restTemplate.exchange("/api/reservations/{reservationId}",
+
+            String username = "owner@gmail.com";
+            String jwtToken = tokenUtils.generateToken(username);
+
+            // Postavljanje tokena u zaglavlje
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + jwtToken);
+
+            // Stvaranje cijele rezervacije za slanje u tijelu zahtjeva
+            ReservationDTO reservationDTO = new ReservationDTO(reservationId, mockStartDate, mockEndDate, 400.0, 3L, 3L, 2, ReservationStatus.APPROVED);
+
+
+
+            // Slanje HTTP PUT zahtjeva s cijelom rezervacijom u tijelu zahtjeva
+            ResponseEntity<ReservationDTO> responseEntity = restTemplate.exchange(
+                    "/api/reservations/{reservationId}",
                     HttpMethod.PUT,
-                    requestEntity,
+                    new HttpEntity<>(reservationDTO, headers),
                     ReservationDTO.class,
-                    reservationDTO.getId());
+                    reservationId
+            );
 
-
-//            ReservationDTO updatedReservation = responseEntity.getBody();
-
-            assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-//            assertEquals(updatedReservation.getReservationStatus(), "APPROVED");
+            // Provjera očekivanog status koda
+            assertEquals(expectedStatusCode, responseEntity.getStatusCodeValue());
         }
 
-        private ReservationDTO createSampleReservation() {
-            // Implementirajte logiku za kreiranje rezervacije za test
-            return new ReservationDTO(1L, null, null, 100.0, 1L, 1L, 1, ReservationStatus.CREATED);
-
+        // Metoda koja generira testne podatke
+        private static Stream<Arguments> provideTestCases() {
+            return Stream.of(
+                    Arguments.of(8L, 200),  // Postojeći ID, očekivani status 200 (OK)
+                    Arguments.of(999L, 404)  // Nepostojeći ID, očekivan rezultat 404 (NOT FOUND)
+                    // Dodajte dodatne argumente prema potrebi
+            );
         }
 
-        private ReservationDTO createSampleUpdatedReservation(ReservationDTO initialReservation) {
-            // Implementirajte logiku za ažuriranje rezervacije za test
-            ReservationDTO updatedReservation = new ReservationDTO();
-            updatedReservation.setId(initialReservation.getId());
-            updatedReservation.setReservationStatus(ReservationStatus.APPROVED);
-            // Dodajte ostale ažurirane podatke ako je potrebno
-            return updatedReservation;
-        }
+
     }
 
