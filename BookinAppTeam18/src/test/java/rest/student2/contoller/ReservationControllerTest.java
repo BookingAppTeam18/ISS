8b13d727c1f398c2a1d5baf4423656a95b7bf169
ZivanicBojan <zivanic.sv61.2021@uns.ac.rs>
2024-01-19 20:26:44 +0100

    Finished controller testing

--- BookinAppTeam18/src/test/java/rest/student2/contoller/ReservationControllerTest.java
+++ BookinAppTeam18/src/test/java/rest/student2/contoller/ReservationControllerTest.java
@@ -1,7 +1,7 @@
-package rest.student2.controller;
+package rest.student2.contoller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -9,17 +9,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import rest.controller.ReservationController;
+import rest.domain.DTO.PriceDTO;
 import rest.domain.DTO.ReservationDTO;
 import rest.domain.enumerations.ReservationStatus;
+import rest.service.PriceService;
 import rest.service.ReservationService;
 
 import java.util.Calendar;
 import java.util.Date;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ReservationController.class)
 public class ReservationControllerTest {
@@ -27,7 +32,7 @@ public class ReservationControllerTest {
     @MockBean
     private ReservationService reservationService;
 
-    // Remove unnecessary @Autowired annotation
+    @Autowired
     private ObjectMapper objectMapper;
 
     @Autowired
@@ -35,11 +40,12 @@ public class ReservationControllerTest {
 
     @Test
     @DisplayName("Should change reservation status")
+    @WithUserDetails("owner")
     public void shouldChangeReservationStatus() throws Exception {
         Date mockStartDate = new Date();
         Date mockEndDate = new Date(mockStartDate.getTime());
 
-        // Add a week to the end date
+        // Dodajte nedelju dana na datum završetka
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(mockEndDate);
         calendar.add(Calendar.DAY_OF_MONTH, 7);
@@ -49,24 +55,23 @@ public class ReservationControllerTest {
 
         ReservationDTO updatedReservationDTO = new ReservationDTO(1L, mockStartDate, mockEndDate, 100.0, 1L, 1L, 1, ReservationStatus.APPROVED);
 
-        if (reservationService != null) {
-            Mockito.when(reservationService.findOne(reservationDTO.getId())).thenReturn(reservationDTO);
-            Mockito.when(reservationService.update(reservationDTO)).thenReturn(updatedReservationDTO);
-        }
+        // Postavite ponašanje za findOne i update
+        Mockito.when(reservationService.findOne(reservationDTO.getId())).thenReturn(reservationDTO);
+        Mockito.when(reservationService.update(Mockito.any(ReservationDTO.class))).thenReturn(updatedReservationDTO);
 
+        // Ispitivanje ažuriranja
         mockMvc.perform(put("/api/reservations/{reservationId}", reservationDTO.getId())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(reservationDTO)))
-                .andExpect(status().is(200))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.size()", Matchers.is(1)))
-                .andExpect(jsonPath("$[0].id", Matchers.is(1)))
-                .andExpect(jsonPath("$[0].startDate", Matchers.is(mockStartDate.getTime())))
-                .andExpect(jsonPath("$[0].endDate", Matchers.is(mockEndDate.getTime())))
-                .andExpect(jsonPath("$[0].price", Matchers.is(100.0)))
-                .andExpect(jsonPath("$[0].accountId", Matchers.is(1L)))
-                .andExpect(jsonPath("$[0].accommodationId", Matchers.is(1L)))
-                .andExpect(jsonPath("$[0].numberOfGuests", Matchers.is(1)))
-                .andExpect(jsonPath("$[0].reservationStatus", Matchers.is(ReservationStatus.APPROVED)));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.startDate").value(mockStartDate))
+                .andExpect(jsonPath("$.endDate").value(mockEndDate))
+                .andExpect(jsonPath("$.price").value(100.0))
+                .andExpect(jsonPath("$.accountId").value(1L))
+                .andExpect(jsonPath("$.accommodationId").value(1L))
+                .andExpect(jsonPath("$.numberOfGuests").value(1))
+                .andExpect(jsonPath("$.reservationStatus").value(ReservationStatus.APPROVED.toString()));
     }
 }
